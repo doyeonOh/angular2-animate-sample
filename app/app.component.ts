a0594af0a7c0ca487fa28cd2bb3177635a58552f
DoyeonOh <odyody12@gmail.com>
2016-06-08 13:08:02 +0900

    angular2 snippet 작성 완료

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from 'angular2/core';
+
+
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div class="container">
+    </div>
+  `
+})
+
+export class AppComponent{
+
+}
