7189f4516061da53e824deae9486f88c7d51aea7
DoyeonOh <odyody12@gmail.com>
2016-06-13 17:20:06 +0900

    1차 add Image animation

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,7 @@ import { RouteConfig, ROUTER_DIRECTIVES } from 'angular2/router';
 import { Page1Component } from './page-transition/page1.component';
 import { Page2Component } from './page-transition/page2.component';
 import { MovetextComponent } from './move-text/move-text.component';
+import { MoveImageComponent } from './move-image/move-image.component';
 
 
 @Component({
@@ -10,19 +11,25 @@ import { MovetextComponent } from './move-text/move-text.component';
   template: `
     <div class="container">
       <h1 class="title">Animate example</h1>
-      <div>
-        <p>페이지 변형 | Page Transition | <a href="http://www.waynehong.com/javascript/angular2-page-transition-animation-example/">참조</a></p>
-        <p><a [routerLink]="['PAGE1']">Page 1</a> | <a [routerLink]="['PAGE2']">Page 2</a></p>
-      </div>
-      <div>
-        <p>텍스트 움직이기 | Move Text</p>
-        <p><a [routerLink]= "['MoveText']">Move Text</a></p>
-      </div>
+      <section>
+        <div>
+          <p>페이지 변형 | Page Transition | <a href="http://www.waynehong.com/javascript/angular2-page-transition-animation-example/">참조</a></p>
+          <p><a [routerLink]="['PAGE1']">Page 1</a> | <a [routerLink]="['PAGE2']">Page 2</a></p>
+        </div>
+        <div>
+          <p>텍스트 움직이기 | Move Text</p>
+          <p><a [routerLink]= "['MoveText']">Move Text</a></p>
+        </div>
+        <div>
+          <p> 이미지 움직이기 | Move Image </p>
+          <p><a [routerLink]= "['MoveImage']">Move Image</a></p>
+        </div>
+      </section>
       <hr/>
-      <div >
+      <section>
         <!-- main contents -->
         <router-outlet></router-outlet>
-      </div>
+      </section>
     </div>
   `,
   directives: [
@@ -33,7 +40,8 @@ import { MovetextComponent } from './move-text/move-text.component';
 @RouteConfig([
   { path: '/page1', name: 'PAGE1', component: Page1Component },
   { path: '/page2', name: 'PAGE2', component: Page2Component },
-  { path: '/moveText', name: 'MoveText', component: MovetextComponent }
+  { path: '/moveText', name: 'MoveText', component: MovetextComponent },
+  { path: '/moveImage', name: 'MoveImage', component: MoveImageComponent }
 ])
 
 export class AppComponent{
---
+++ app/move-image/move-image.component.ts
@@ -0,0 +1,44 @@
+import { Component} from 'angular2/core';
+
+@Component({
+  selector: 'move-image',
+  template: `
+  <div class="demo">
+    <div class="frame">
+
+    </div>
+  </div>
+  `,
+  styles: [`
+    .demo{
+      width:264px;
+      height:525px;
+      position: absolute;
+    }
+
+    .frame{
+      width: 100%;
+      height: 100%;
+      background: url('/resources/img/frame.png') no-repeat center;
+      position: absolute;
+    }
+
+    :host(.anitest) {
+      position: absolute;
+      transition: all ease-in 0.5s;
+    }
+
+    :host(.anitest.ng-enter) {
+      transform: translateY(-1in)
+    }
+
+    :host(.anitest.ng-enter-active) {
+      transform: none;
+
+    }
+  `],
+  host: {'class': 'ng-animate anitest'}
+})
+
+export class MoveImageComponent {
+}
